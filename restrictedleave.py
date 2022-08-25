a71cc1041a67258fad0c3582e66367cf4fb85d54
ROHINISARASWATHI <105505290+ROHINISARASWATHI@users.noreply.github.com>
2022-08-25 07:48:35 -0400

    Delete restrictedleave.py

--- restrictedleave.py
+++
@@ -1,7 +0,0 @@
-from leave import leave
-class restrictedLeave(leave):
-    def _init_(self,employeeid,name,leavebalance,DateofBirth):
-        super()._init_(employeeid, name, leavebalance)
-        self.dob=DateofBirth
-    def ApplyLeave(self):
-        return self.name,self.dob
